2932b4e3bfd8957358a8e1615fdd32a261266c6a
ExecuteOrd66 <57319507+ExecuteOrd66@users.noreply.github.com>
2026-02-06 21:19:30 +0200

    Restore legacy ORMLite DatabaseHelper abstractions

---
+++ app/src/main/java/ca/fuwafuwa/gaku/Database/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package ca.fuwafuwa.gaku.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.sql.SQLException;
+
+public abstract class DatabaseHelper extends OrmLiteSqliteOpenHelper implements IDatabaseHelper {
+
+    public DatabaseHelper(Context context, String databaseName, SQLiteDatabase.CursorFactory factory, int databaseVersion) {
+        super(context, databaseName, factory, databaseVersion);
+    }
+
+    @Override
+    public abstract void onCreate(SQLiteDatabase database, ConnectionSource connectionSource);
+
+    @Override
+    public abstract void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion);
+
+    public abstract void deleteDatabase();
+
+    @Override
+    public abstract <T> Dao<T, Integer> getDbDao(Class clazz) throws SQLException;
+}
